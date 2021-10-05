6bbd30d7fd94753ad82146465350c71c55868683
MERYEM-ESSM <mariamtouimi99@gmail.com>
2021-10-05 13:43:34 +0200

    editing css1

--- js/file.js
+++ js/file.js
@@ -1,4 +1,24 @@
 
+var app = document.querySelector('.txt-animation');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(250)
+  .typeString('I am MERYEM ESSM,a developer')
+  .pauseFor(300)
+  .deleteChars(11)
+  .typeString('a<strong> front-end </strong>developer ')
+ 
+  .pauseFor(1000)
+  .start();
+
+
+
+
 
 
 
